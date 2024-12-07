2e93403bd57f7595d337db805f2a89f0bea12e3d
nikhilgangrial <nikhilgangrial@gmail.com>
2024-12-07 15:36:47 +0530

    add grpc example code

--- expense-app-apis/src/main.rs
+++ expense-app-apis/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use tonic::transport::Server;
+
+mod services;
+
+use services::calculator::{CalculatorServer, CalculatorService, CALC_DESCRIPTOR};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = "[::1]:50069".parse()?;
+
+    let calc = CalculatorService {};
+
+    let reflection_service = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(CALC_DESCRIPTOR)
+        .build_v1()?;
+
+    Server::builder()
+        .add_service(reflection_service)
+        .add_service(CalculatorServer::new(calc))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
---
+++ expense-app-apis/src/services/calculator.rs
@@ -0,0 +1,33 @@
+mod proto {
+    tonic::include_proto!("calculator");
+
+    pub const CALC_DESCRIPTOR: &[u8] = tonic::include_file_descriptor_set!("calculator_descriptor");
+}
+
+use tonic::{Request, Response, Status};
+
+use proto::{calculator_server::Calculator, CalculationRequest, CalculationResponse};
+
+pub use proto::{calculator_server::CalculatorServer, CALC_DESCRIPTOR};
+
+pub struct CalculatorService;
+
+#[tonic::async_trait]
+impl Calculator for CalculatorService {
+    async fn divide(
+        &self,
+        request: Request<CalculationRequest>,
+    ) -> Result<Response<CalculationResponse>, Status> {
+        let input = request.get_ref();
+
+        if input.b == 0 {
+            return Err(Status::invalid_argument("Cannot divide by zero"));
+        }
+
+        let response = CalculationResponse {
+            result: input.a / input.b,
+        };
+
+        Ok(Response::new(response))
+    }
+}
