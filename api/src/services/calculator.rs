b98a97391d9dd8949c6c20ef3a2685194af2537f
nikhilgangrial <nikhilgangrial@gmail.com>
2025-01-04 15:00:26 +0530

    Add logging using tracing

--- api/src/services/calculator.rs
+++ api/src/services/calculator.rs
@@ -3,22 +3,27 @@ mod proto {
 }
 
 use tonic::{Request, Response, Status};
+use tracing::{debug, info};
 
 use proto::{calculator_server::Calculator, CalculationRequest, CalculationResponse};
 
 pub use proto::calculator_server::CalculatorServer;
 
+#[derive(Debug)]
 pub struct CalculatorService;
 
 #[tonic::async_trait]
 impl Calculator for CalculatorService {
+    #[tracing::instrument(skip_all)]
     async fn divide(
         &self,
         request: Request<CalculationRequest>,
     ) -> Result<Response<CalculationResponse>, Status> {
+        info!("received request {:?}", request);
         let input = request.get_ref();
 
         if input.b == 0 {
+            debug!("Division by zero");
             return Err(Status::invalid_argument("Cannot divide by zero"));
         }
 
@@ -26,6 +31,7 @@ impl Calculator for CalculatorService {
             result: input.a / input.b,
         };
 
+        info!("returning response {:?}", response);
         Ok(Response::new(response))
     }
 }
